5e8c7645883bf4e8f5437d5822879d8e9ac1c3a7
chenleo <chenleo@sea.com>
2022-08-29 17:32:51 +0800

    QuickSort

--- src/main/java/utils.java
+++ src/main/java/utils.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class utils {
@@ -8,10 +9,7 @@ public class utils {
     }
 
     public static void printList(int[] arr) {
-        for (int x : arr) {
-            System.out.print(x + ", ");
-        }
-        System.out.println();
+        System.out.println(Arrays.toString(arr));
     }
 
     public static int[] generateRandomArray(int n) {
