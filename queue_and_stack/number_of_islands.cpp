d4f27572576c6805c49cfc54713e5d008ade622d
leochen <event10342012@gmail.com>
2023-08-21 22:25:22 +0800

    add sort algorithm

---
+++ queue_and_stack/number_of_islands.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Leo Chen on 2023/8/21.
+//
+
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>> &grid) {
+
+    }
+};
--- queue_and_stack/wall_and_gate.cpp
+++ queue_and_stack/wall_and_gate.cpp
@@ -2,3 +2,56 @@
 // Created by Leo Chen on 2023/8/8.
 //
 
+#include <climits>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+private:
+    const static int empty = INT_MAX;
+    const static int gate = 0;
+
+public:
+    void wallsAndGates(vector<vector<int> > &rooms) {
+        const static vector<vector<int>> directions = {
+                vector<int>({1, 0}),
+                vector<int>({-1, 0}),
+                vector<int>({0, 1}),
+                vector<int>({0, -1})
+        };
+
+        unsigned long m = rooms.size();
+        if (m == 0) return;
+        unsigned long n = rooms[0].size();
+        queue<vector<int>> q;
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (rooms[row][col] == gate) {
+                    q.push(vector<int>({row, col}));
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            vector<int> point = q.front();
+            q.pop();
+
+            int row = point[0];
+            int col = point[1];
+
+            for (vector<int> direction: directions) {
+                int r = row + direction[0];
+                int c = col + direction[1];
+
+                if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != empty) {
+                    continue;
+                }
+                rooms[r][c] = rooms[row][col] + 1;
+                q.push(vector<int>({r, c}));
+            }
+        }
+    }
+};
